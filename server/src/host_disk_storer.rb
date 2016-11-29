ba3be2440f23fe53d51acccead53f6e1a15eeb83
Jon Jagger <jon@jaggersoft.com>
2016-11-29 17:23:33 +0000

    addes start of kata.exists?(id) and tests

--- server/src/host_disk_storer.rb
+++ server/src/host_disk_storer.rb
@@ -13,13 +13,41 @@ class HostDiskStorer
     @path ||= ENV['CYBER_DOJO_KATAS_ROOT']
   end
 
-
+  def kata_exists?(id)
+    valid?(id) #&& kata_dir(id).exists?
+  end
 
   private
 
-  def disk; nearest_external(:disk); end
-  def git; nearest_external(:git); end
+  #def kata_dir(id)
+  #  disk[kata_path(id)]
+  #end
 
-  include NearestExternal
+  #def kata_path(id)
+  #  path + '/' + outer(id) + '/' + inner(id)
+  #end
+
+  def valid?(id)
+    id.class.name == 'String' &&
+      id.length == 10 &&
+        id.chars.all? { |char| hex?(char) }
+  end
+
+  def hex?(char)
+    '0123456789ABCDEF'.include?(char)
+  end
+
+  #def outer(id)
+  #  id.upcase[0..1]  # 'E5'
+  #end
+
+  #def inner(id)
+  #  id.upcase[2..-1] # '6A3327FE'
+  #end
+
+  #def disk; nearest_external(:disk); end
+  #def git; nearest_external(:git); end
+
+  #include NearestExternal
 
 end
--- server/test/src/host_disk_storer_test.rb
+++ server/test/src/host_disk_storer_test.rb
@@ -5,6 +5,8 @@ class HostDiskStorerTest < StorerTestBase
   def self.hex_prefix; 'E4FDA'; end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # parent
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '72D',
   "Storer's parent object is the test object" do
@@ -12,11 +14,46 @@ class HostDiskStorerTest < StorerTestBase
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # path
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '218',
   'path is set to /tmp in docker-compose.yml' do
     assert_equal '/tmp', storer.path
   end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # kata_exists?(id)
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'D0A',
+  'kata_exists?(id) for id that is not a string is false' do
+    not_string = Object.new
+    refute storer.kata_exists?(not_string)
+  end
+
+  test '55F',
+  'kata_exists?(id) for string not 10 chars long is false' do
+    nine = '60408161A'
+    assert_equal 9, nine.length
+    refute storer.kata_exists?(nine)
+  end
+
+  test '8F9',
+  'kata_exists?(id) for string 10 chars long but not all hex is false' do
+    ten = '60408161AG'
+    assert_equal 10, ten.length
+    refute storer.kata_exists?(ten)
+  end
+
+=begin
+  test '79A',
+  'kata_exists?(id) false' do
+    ten = 'CE121BE38A'
+    assert_equal 10, ten.length
+    refute storer.kata_exists?(ten)
+  end
+=end
 
   private
 
