75d9f65c16c150bf309c52bf47478a1424524324
Jon Jagger <jon@jaggersoft.com>
2016-11-29 08:10:25 +0000

    set server base-line metrics limits

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -97,12 +97,12 @@ table =
     [ 'failures',               failure_count,      '==',   0 ],
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
-    [ 'assertions/s',           assertions_per_sec, '>=',   1 ],
-    [ 'duration(test)[s]',      test_duration,      '<=', 120 ],
+    [ 'assertions/s',           assertions_per_sec, '>=', 100 ],
+    [ 'duration(test)[s]',      test_duration,      '<=',   5 ],
     [ 'coverage(src)[%]',       src_coverage,       '>=', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '>=', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 200 ],
-    [ 'hits_per_line(test)',    hits_per_line_test, '<=',   5 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=',  10 ],
+    [ 'hits_per_line(test)',    hits_per_line_test, '<=',   3 ],
     [ 'lines(test)/lines(src)', f2(line_ratio),     '>=',   2 ],
   ]
 
