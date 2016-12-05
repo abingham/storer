b7e83a6e2668f7e3b2993d4b95538e1b5c4c3376
Jon Jagger <jon@jaggersoft.com>
2016-12-05 19:16:51 +0000

    refactor; remove duplication

--- client/src/storer_http_adapter.rb
+++ client/src/storer_http_adapter.rb
@@ -4,46 +4,54 @@ require 'net/http'
 class StorerHttpAdapter
 
   def create_kata(manifest)
-    post(__method__, { manifest:manifest })
+    post(__method__, manifest)
   end
 
   def kata_exists(kata_id)
-    get(__method__, { kata_id:kata_id })
+    get(__method__, kata_id)
   end
 
   def kata_manifest(kata_id)
-    get(__method__, { kata_id:kata_id })
+    get(__method__, kata_id)
   end
 
   # - - - - - - - - - - - -
 
   def completed(id)
-    get(__method__, { id:id })
+    get(__method__, id)
   end
 
   def completions(id)
-    get(__method__, { id:id })
+    get(__method__, id)
   end
 
   # - - - - - - - - - - - -
 
   def kata_started_avatars(kata_id)
-    get(__method__, { kata_id:kata_id })
+    get(__method__, kata_id)
   end
 
   def kata_start_avatar(kata_id, avatar_names)
-    post(__method__, { kata_id:kata_id, avatar_names:avatar_names })
+    post(__method__, kata_id, avatar_names)
   end
 
 
   private
 
-  def get(method, args)
-    http(method, args) { |uri| Net::HTTP::Get.new(uri) }
+  def get(method, *args)
+    http(method, args_hash(method, *args)) { |uri| Net::HTTP::Get.new(uri) }
   end
 
-  def post(method, args)
-    http(method, args) { |uri| Net::HTTP::Post.new(uri) }
+  def post(method, *args)
+    http(method, args_hash(method, *args)) { |uri| Net::HTTP::Post.new(uri) }
+  end
+
+  def args_hash(method, *args)
+    hash = {}
+    self.class.instance_method(method).parameters.each_with_index do |entry,index|
+      hash[entry[1]] = args[index]
+    end
+    hash
   end
 
   def http(method, args)
