4c12ea3044917ff4aa8e269767ca35324f2eb921
Jon Jagger <jon@jaggersoft.com>
2016-12-06 08:47:59 +0000

    refactoring

--- client/src/storer_http_adapter.rb
+++ client/src/storer_http_adapter.rb
@@ -57,26 +57,23 @@ module StorerHttpAdapter # mix-in
 
   def get(method, *args)
     name = method.to_s
-    json = http(method, args_hash(method, *args)) { |uri| Net::HTTP::Get.new(uri) }
+    json = http(name, args_hash(name, *args)) { |uri| Net::HTTP::Get.new(uri) }
     json[name]
   end
 
   def post(method, *args)
     name = method.to_s
-    json = http(method, args_hash(method, *args)) { |uri| Net::HTTP::Post.new(uri) }
+    json = http(name, args_hash(name, *args)) { |uri| Net::HTTP::Post.new(uri) }
     json[name]
   end
 
   def args_hash(method, *args)
-    hash = {}
-    self.class.instance_method(method).parameters.each_with_index do |entry,index|
-      hash[entry[1]] = args[index]
-    end
-    hash
+    parameters = self.class.instance_method(method).parameters
+    Hash[parameters.map.with_index { |parameter,index| [parameter[1], args[index]] }]
   end
 
   def http(method, args)
-    uri = URI.parse('http://storer_server:4577/' + method.to_s)
+    uri = URI.parse('http://storer_server:4577/' + method)
     http = Net::HTTP.new(uri.host, uri.port)
     request = yield uri.request_uri
     request.content_type = 'application/json'
