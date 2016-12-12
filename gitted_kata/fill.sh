31c3a9e1bab1c2776ccd710ee09a0e1dc243c80d
Jon Jagger <jon@jaggersoft.com>
2016-12-12 11:09:22 +0000

    travis is running docker 1.12.3, so am I :-( ... trying different cp command

--- gitted_kata/fill.sh
+++ gitted_kata/fill.sh
@@ -7,7 +7,7 @@ CYBER_DOJO_GITTED_KATAS_ROOT=/tmp/katas
 CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER=cyber-dojo-gitted-katas-DATA-CONTAINER
 
 docker cp \
-  ${MY_DIR}/5A/. \
+  ${MY_DIR}/5A \
   ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER}:${CYBER_DOJO_GITTED_KATAS_ROOT}/5A
 
 # can't exec directly into container as it is not running
