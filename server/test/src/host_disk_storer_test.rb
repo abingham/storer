a82d65615010ea533155d91170ac97361e9d4b19
Jon Jagger <jon@jaggersoft.com>
2016-12-12 13:05:40 +0000

    removed puts from gitted-kata test

--- server/test/src/host_disk_storer_test.rb
+++ server/test/src/host_disk_storer_test.rb
@@ -325,7 +325,6 @@ class HostDiskStorerTest < StorerTestBase
     assert avatar_dir.exists?
     git_path = avatar_path + '/.git'
     git_dir = disk[git_path]
-    puts `cd #{avatar_path} && ls -al`
     assert git_dir.exists?
   end
 
