4f17473c66a3337ba2a9ab4c33ec373cc0be4bbc
Jon Jagger <jon@jaggersoft.com>
2016-12-12 11:24:30 +0000

    added more logging to help debug travis

--- gitted_kata/build.sh
+++ gitted_kata/build.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 set -e
 
+echo 0
+docker volume ls
+
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 CONTEXT_DIR=${MY_DIR}
 TAG=cyberdojo/gitted_katas
 CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER=cyber-dojo-gitted-katas-DATA-CONTAINER
 
-(docker rm -f ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER}) || true
+(docker rm --force --volumes ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER}) || true
+
+echo 1
+docker volume ls
 
 cd ${MY_DIR}
 
 docker build \
-          --build-arg=CYBER_DOJO_GITTED_KATAS_ROOT=/tmp/katas \
-          --tag=${TAG} \
-          --file=Dockerfile \
-          ${CONTEXT_DIR}
+  --build-arg=CYBER_DOJO_GITTED_KATAS_ROOT=/tmp/katas \
+  --tag=${TAG} \
+  --file=Dockerfile \
+  ${CONTEXT_DIR}
+
+echo 2
+docker volume ls
 
 CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER=cyber-dojo-gitted-katas-DATA-CONTAINER
 
 docker create \
-          --name ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER} \
-          ${TAG} \
-          echo 'cdfGittedKatasDC' > /dev/null
+  --name ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER} \
+  ${TAG} \
+  echo 'cdfGittedKatasDC' > /dev/null
+
+echo 3
+docker volume ls
+
+docker run \
+  --rm \
+  --tty \
+  --volumes-from ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER} \
+  cyberdojo/ruby:latest \
+  sh -c "cd /tmp/katas && ls -al"
--- gitted_kata/fill.sh
+++ gitted_kata/fill.sh
@@ -7,20 +7,24 @@ CYBER_DOJO_GITTED_KATAS_ROOT=/tmp/katas
 CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER=cyber-dojo-gitted-katas-DATA-CONTAINER
 
 docker cp \
-  ${MY_DIR}/5A \
+  ${MY_DIR}/5A/. \
   ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER}:${CYBER_DOJO_GITTED_KATAS_ROOT}/5A
 
 # can't exec directly into container as it is not running
 
 docker pull cyberdojo/ruby:latest
 
-docker run --rm -it \
-  --volumes-from cyber-dojo-gitted-katas-DATA-CONTAINER \
+docker run \
+  --rm \
+  --tty \
+  --volumes-from ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER} \
   cyberdojo/ruby:latest \
   sh -c "chown -R cyber-dojo /tmp/katas"
 
-docker run --rm -it \
-  --volumes-from cyber-dojo-gitted-katas-DATA-CONTAINER \
+docker run \
+  --rm \
+  --tty \
+  --volumes-from ${CYBER_DOJO_GITTED_KATAS_DATA_CONTAINER} \
   cyberdojo/ruby:latest \
   sh -c "cd /tmp/katas/5A/0F824303/spider && ls -al"
 
