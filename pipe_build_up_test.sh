85f671c224ec2c3f5afcabfe57ac41b12967b49e
Jon Jagger <jon@jaggersoft.com>
2017-01-02 07:25:43 +0000

    moved git_kata/ into server/

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,8 +3,8 @@ set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-${my_dir}/git_kata/build.sh
-${my_dir}/git_kata/fill.sh
+${my_dir}/server/git_kata/build.sh
+${my_dir}/server/git_kata/fill.sh
 
 ${my_dir}/build.sh
 ${my_dir}/up.sh
