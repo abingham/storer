9c720a98b4b0a89602abcad25e68721aabf5c723
Jon Jagger <jon@jaggersoft.com>
2017-03-04 21:12:54 +0000

    moved commands to build git-kata-container into build.sh so pipe_build_up_test.sh works when run alone

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,9 +3,6 @@ set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-${my_dir}/server/git_kata/build.sh
-${my_dir}/server/git_kata/fill.sh
-
 ${my_dir}/build.sh
 ${my_dir}/up.sh
 ${my_dir}/test.sh ${*}
