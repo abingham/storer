836796ec5a43b7caa13c22972fc682497b09daad
Jon Jagger <jon@jaggersoft.com>
2017-09-20 19:29:27 +0200

    refactoring

--- server/test/src/google_cloud_data_storer_test.rb
+++ server/test/src/google_cloud_data_storer_test.rb
@@ -141,6 +141,43 @@ class GoogleCloudDataStorerTest < TestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - -
 
+  test '02A',
+  'completing a 6-digit kata-id to one 10-digit id' do
+    id = '4B80741C3C'
+    kata_create id
+    begin
+      key_prefix = id[0..5]
+      query = datastore.query('kata').where('key_prefix', '=', key_prefix)
+      all = datastore.run(query).to_a
+      assert_equal 1, all.size
+      assert_equal id, all[0].key.name
+    ensure
+      kata_delete id
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '02B',
+  'completing a 6-digit kata-id to two 10-digit ids' do
+    key_prefix = '7658BC'
+    id1 = key_prefix + '1C3C'
+    kata_create id1
+    id2 = key_prefix + '4DE5'
+    kata_create id2
+    begin
+      query = datastore.query('kata').where('key_prefix', '=', key_prefix)
+      all = datastore.run(query).to_a
+      assert_equal 2, all.size
+      assert_equal [id1,id2], all.map{ |e| e.key.name }
+    ensure
+      kata_delete id1
+      kata_delete id2
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - -
+
   def avatar_create(id, name)
     avatar = datastore.entity [['kata', id], ['avatar', name]] do |e|
       e['increments'] = '[]'
@@ -162,206 +199,43 @@ class GoogleCloudDataStorerTest < TestBase
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - -
-  # ...refactored up to here...
-  # - - - - - - - - - - - - - - - - - - - - - - -
 
   test '030',
-  'read based on key' do
-    id = '65CFFFEC38'
-    key = datastore.key [['kata', id], ['avatar', 'lion']]
-    avatar = datastore.find key
-    rags = JSON.parse(avatar['increments'])
-    assert_equal 'Array', rags.class.name
+  'atomically update an avatars increments' do
+    id = 'B7FAA22CCB'
+    avatar_create id, lion
+    begin
+      key = datastore.key [['kata', id], ['avatar', lion]]
+      datastore.transaction do |tx|
+        avatar = tx.find key
+        rags = JSON.parse(avatar['increments'])
+        rags << 'red'
+        avatar['increments'] = JSON.unparse(rags)
+        datastore.save avatar
+      end
+      avatar = avatar_exists? id, lion
+      refute_nil avatar
+      rags = JSON.parse(avatar['increments'])
+      assert_equal ['red'], rags
+    ensure
+      avatar_delete id, lion
+    end
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - -
 
   test '031',
-  'atomically update an avatars increments' do
-    id = 'B7FAA22CCB'
-    key = datastore.key [['kata', id], ['avatar', 'lion']]
-    datastore.transaction do |tx|
-      avatar = tx.find key
+  'read based on key' do
+    id = '65CFFFEC38'
+    avatar_create id, lion
+    begin
+      avatar = avatar_exists? id, lion
+      refute_nil avatar
       rags = JSON.parse(avatar['increments'])
-      rags << 'red'
-      avatar['increments'] = JSON.unparse(rags)
-      datastore.save avatar
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '02A',
-  'completing a 6-digit kata-id to 10-digits' do
-    id = '4B80741C3C'
-    kata_start id
-    begin
-      key_prefix = id[0..5]
-      query = datastore.query('kata').where('key_prefix','=',key_prefix)
-      all = datastore.run query
-      assert_equal 1, all.size
-      assert_equal id, all.to_a[0].key.name
+      assert_equal [], rags
     ensure
-      kata_delete id
+      avatar_delete id, lion
     end
   end
-
-  # - - - - - - - - - - - - - - - - - - - - - - -
-
-=begin
-
-  test '020',
-  'writing data - example from google.cloud web-page' do
-    task = storer.datastore.entity 'Task' do |t|
-      t['description'] = 'hello world'
-      t['created']     = Time.now
-      t['done']        = false
-      t.exclude_from_indexes! 'description', true
-    end
-    storer.datastore.save task
-    assert_equal 'Fixnum', task.key.id.class.name
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '021',
-  'read based on content' do
-    e = storer.datastore.entity 'traffic-light' do |rag|
-      rag['id1'] = '93'
-      rag['id2'] = '63EF'
-      rag['id3'] = '9A5E'
-      rag['colour'] = 'red'
-      rag.exclude_from_indexes! 'colour', true
-    end
-    storer.datastore.save e
-
-    query = storer.datastore.query('traffic-light').
-              where('id1','==','93').
-              where('id2','==','63EF').
-              where('id3','==','9A5E')
-
-    rags = storer.datastore.run query
-    rags.each do |rag|
-      puts "~~~~~"
-      puts rag.properties.to_hash
-      puts "~~~~~"
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '838',
-  'write-read round-trip' do
-    filename = 'nuts.txt'
-    content = 'cashew'
-    storer.write(filename, content)
-    assert_equal content, storer.read(filename)
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '839',
-  'write overwrites previous write' do
-    filename = 'nuts.txt'
-    storer.write(filename, content='cashew')
-    begin
-      assert_equal content, storer.read(filename)
-      storer.write(filename, content='brazil')
-      assert_equal content, storer.read(filename)
-    ensure
-      storer.delete filename
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '83A',
-  'exists?(existing-file) is true' do
-    filename = 'nuts.txt'
-    storer.write(filename, content='cashew')
-    begin
-      assert storer.exists? filename
-    ensure
-      storer.delete filename
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '83B',
-  'exists?(non-existant-file) is false' do
-    refute storer.exists? 'does-not-exist.txt'
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '83C',
-  'delete(existing-file) succeeds and file no longer exists' do
-    filename = 'nuts.txt'
-    storer.write(filename, content='cashew')
-    storer.delete(filename)
-    refute storer.exists? filename
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '83D',
-  'delete(never-existed-file) raises' do
-    filename = '123456321245.txt'
-    assert_raises { storer.delete(filename) }
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '9ED',
-  'files one simple completion' do
-    filename = 'nuts.txt'
-    storer.write(filename, content='cashew')
-    begin
-      files = storer.files('nu')
-      all = files.collect { |file| file.name }
-      assert_equal [ 'nuts.txt' ], all
-    ensure
-      storer.delete filename
-    end
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - -
-
-  test '9EE',
-  'files more than one simple completion' do
-    filename1 = 'fruit.txt'
-    filename2 = 'fruit2.txt'
-    storer.write(filename1, 'bananas')
-    storer.write(filename2, 'apples')
-    begin
-      files = storer.files('fru')
-      all = files.collect { |file| file.name }
-      assert_equal [ filename1, filename2 ].sort, all.sort
-    ensure
-      storer.delete filename1
-      storer.delete filename2
-    end
-  end
-
-  test '9EF',
-  'files more than one not-so-simple completion' do
-    filename1 = '19/23456789/fruit.txt'
-    filename2 = '19/23456789/fruit2.txt'
-    filename3 = '91/23456789/nuts6.txt'
-    storer.write(filename1, 'bananas')
-    storer.write(filename2, 'apples')
-    storer.write(filename3, 'peanut')
-    begin
-      files = storer.files('19/23456789')
-      all = files.collect { |file| file.name }
-      assert_equal [ filename1, filename2 ].sort, all.sort
-    ensure
-      storer.delete filename1
-      storer.delete filename2
-      storer.delete filename3
-    end
-  end
-=end
 
 end
