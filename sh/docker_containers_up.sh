6450848df86fa3e798ecbf5ab5da1ed1ded99763
Jon Jagger <jon@jaggersoft.com>
2017-11-18 13:39:18 +0000

    added crude wait for services to be up before running tests

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -3,4 +3,22 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+check_up()
+{
+  set +e
+  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
+  set -e
+  if [ "${s}" != "${1}" ]; then
+    echo
+    echo "${1} not up"
+    docker logs ${1}
+    exit 1
+  fi
+}
+
 docker-compose --file ${ROOT_DIR}/docker-compose.yml up -d
+
+# crude wait for services
+sleep 2
+check_up 'storer_server'
+check_up 'storer_client'
