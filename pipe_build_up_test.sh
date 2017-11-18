a0efcf577f9788cacf0c0af63f2f8c48d0f24079
Jon Jagger <jon@jaggersoft.com>
2017-11-18 15:36:02 +0000

    prepare files for creating katas volume when making stand-alone storer server

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -4,7 +4,7 @@ set -e
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 ${MY_DIR}/sh/build_docker_images.sh
-${MY_DIR}/server/git_kata/build_data_container_volume.sh
+${MY_DIR}/server/git_kata_volume/build_data_container.sh
 ${MY_DIR}/sh/docker_containers_up.sh
-${MY_DIR}/server/git_kata/pipe_into_volume.sh
+${MY_DIR}/server/git_kata_volume/tar_pipe_in.sh
 ${MY_DIR}/sh/run_tests_in_containers.sh ${*}
