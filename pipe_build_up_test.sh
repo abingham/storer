fab847fdbe8e337d8582a5b7476d5c0b43c1ede1
Jon Jagger <jon@jaggersoft.com>
2017-11-18 18:27:40 +0000

    have to make the data-container first

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,8 +3,8 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-${MY_DIR}/sh/build_docker_images.sh
 ${MY_DIR}/server/git_kata_volume/build_data_container.sh
+${MY_DIR}/sh/build_docker_images.sh
 ${MY_DIR}/sh/docker_containers_up.sh
 ${MY_DIR}/server/git_kata_volume/tar_pipe_in.sh
 ${MY_DIR}/sh/run_tests_in_containers.sh ${*}
