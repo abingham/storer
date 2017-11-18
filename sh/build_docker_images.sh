fa74b54f331f0dbfcd5e74a03e2ee7bb234021c5
Jon Jagger <jon@jaggersoft.com>
2017-11-18 15:29:45 +0000

    refactoring

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -3,6 +3,4 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-${ROOT_DIR}/server/git_kata/build_katas_data_container_volume.sh
-
 docker-compose --file ${ROOT_DIR}/docker-compose.yml build
