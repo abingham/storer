3166d0d0ef858c214f2f8400fb637dc2f3197ae6
Jon Jagger <jon@jaggersoft.com>
2017-11-18 15:10:07 +0000

    changed tack; dont put docker inside storer containers; instead create katas data-volume in the next-outer context, which is either commander for regular use, or could be a custom script if you want to try and run storer in its own VM with its own IP address

--- server/up.sh
+++ server/up.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# verify that the cyber-dojo user can run docker-commands
-# Fails on Travis
-# docker ps -a
-
 # The --host is needed for IPv4 and IPv6 addresses
 bundle exec rackup \
   --warn \
--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -22,10 +22,3 @@ docker-compose --file ${ROOT_DIR}/docker-compose.yml up -d
 sleep 5
 check_up 'storer_server'
 check_up 'storer_client'
-
-echo '~~~~~~~~~/var/run/docker.sock inside the storer...'
-docker exec -it storer_server sh -c 'ls -al /var/run/docker.sock'
-echo '~~~~~~~~~/var/run/docker/sock on the host...'
-#docker-machine ssh default ls -al /var/run/docker.sock
-ls -al /var/run/docker.sock
-echo '~~~~~~~~~'
