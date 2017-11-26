02d7aa310a748ac8d6fbcef146412fee367c6ade
Jon Jagger <jon@jaggersoft.com>
2017-11-26 16:10:40 +0000

    refactoring; test improvements, put hex-mini-test into test/src to match other repos

--- client/test/check_test_results.rb
+++ client/test/check_test_results.rb
@@ -105,8 +105,8 @@ table =
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
     [ 'hits_per_line(src)',     hits_per_line_src,  '<=',  15 ],
-    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 2.6 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.9 ],
+    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 3.8 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.6 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -105,8 +105,8 @@ table =
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
     [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 210 ],
-    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 5.8 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.0 ],
+    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 9.7 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.2 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
