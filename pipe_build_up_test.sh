517ebe9a16858a84061097f6e4e539eba7affe97
Jon Jagger <jon@jaggersoft.com>
2017-12-20 10:43:16 +0000

    made sure katas-data-container is created first

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -7,8 +7,8 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 . ${MY_DIR}/env.${PARAM}
 . ${MY_DIR}/env.port
 
-${MY_DIR}/sh/build_docker_images.sh ${PARAM}
 ${MY_DIR}/server/katas_volume/build_data_container.sh ${PARAM}
+${MY_DIR}/sh/build_docker_images.sh ${PARAM}
 ${MY_DIR}/server/katas_old/tar_pipe_in.sh ${PARAM}
 ${MY_DIR}/sh/docker_containers_up.sh ${PARAM}
 ${MY_DIR}/sh/run_tests_in_containers.sh ${*}
