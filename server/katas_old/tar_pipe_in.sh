54232435ad1212d8437e04de01135917f745d04f
Jon Jagger <jon@jaggersoft.com>
2017-12-20 09:48:07 +0000

    set ownership of test-data in storer's katas data-container

--- server/katas_old/tar_pipe_in.sh
+++ server/katas_old/tar_pipe_in.sh
@@ -9,6 +9,7 @@ readonly KATA_IDS=(5A0F824303 420B05BA0A 420F2A2979 421F303E80 420BD5D5BE 421AFD
 
 . ${MY_DIR}/../../env.${PARAM}
 
+# tar-pipe test data into storer's katas data-container
 for KATA_ID in "${KATA_IDS[@]}"
 do
   cat ${MY_DIR}/${KATA_ID}.tgz \
@@ -16,7 +17,13 @@ do
         --rm \
         --interactive \
         --volumes-from ${CYBER_DOJO_KATA_DATA_CONTAINER_NAME}:rw \
-        alpine:latest \
-          sh -c "tar -zxf - -C ${CYBER_DOJO_KATAS_ROOT}"
+          alpine:latest \
+            sh -c "tar -zxf - -C ${CYBER_DOJO_KATAS_ROOT}"
 done
 
+# set ownership of test-data in storer's katas data-container
+docker run \
+  --rm \
+  --volumes-from ${CYBER_DOJO_KATA_DATA_CONTAINER_NAME} \
+    cyberdojo/storer \
+      sh -c "cd /tmp/katas && chown -R cyber-dojo:cyber-dojo *"
