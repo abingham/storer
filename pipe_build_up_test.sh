f3ff5b9553524edd7cb224d170bbaeb56720b2fe
Jon Jagger <jon@jaggersoft.com>
2017-12-20 06:55:38 +0000

    refactoring; rename dirs

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -10,6 +10,6 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 ${MY_DIR}/server/katas_volume/build_data_container.sh ${PARAM}
 ${MY_DIR}/sh/build_docker_images.sh ${PARAM}
 ${MY_DIR}/sh/docker_containers_up.sh ${PARAM}
-${MY_DIR}/server/git_kata_volume/tar_pipe_in.sh ${PARAM}
+${MY_DIR}/server/kata_old_git/tar_pipe_in.sh ${PARAM}
 ${MY_DIR}/sh/run_tests_in_containers.sh ${*}
 ${MY_DIR}/sh/docker_containers_down.sh ${PARAM}
