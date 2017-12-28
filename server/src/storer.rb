0bf9ef15294431e9bd2c1a541cc6ba6a2e92faa3
Jon Jagger <jon@jaggersoft.com>
2017-12-28 07:25:08 +0000

    refactoring; deleted dead code

--- server/src/storer.rb
+++ server/src/storer.rb
@@ -195,13 +195,6 @@ class Storer
     }
   end
 
-  # - - - - - - - - - - - - - - - - - - -
-
-  def tag_fork(kata_id, avatar_name, tag)
-    assert_tag_exists(kata_id, avatar_name, tag)
-    #TODO
-  end
-
   private # = = = = = = = = = = = = = = = = = = = = =
 
   attr_reader :disk, :shell
