cd12b46e93b0a87b4dcf589e8d3475f37db617b7
Jon Jagger <jon@jaggersoft.com>
2018-01-03 14:15:22 +0000

    refactoring; renamed file

--- sh/docker_containers_down.sh
+++ sh/docker_containers_down.sh
@@ -3,7 +3,7 @@
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
 . ${ROOT_DIR}/env.test
-. ${ROOT_DIR}/env.port
+. ${ROOT_DIR}/env.common
 
 docker-compose \
   --file ${ROOT_DIR}/docker-compose.yml \
--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -4,7 +4,7 @@ set -e
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
 . ${ROOT_DIR}/env.test
-. ${ROOT_DIR}/env.port
+. ${ROOT_DIR}/env.common
 
 docker-compose \
   --file ${ROOT_DIR}/docker-compose.yml \
