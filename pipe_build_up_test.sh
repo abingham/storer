4726b8e2d49a5915a17d9751a7d291a42e536f93
Jon Jagger <jon@jaggersoft.com>
2018-04-29 15:50:54 +0100

    refactoring; put quotes around use of bash variables

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-export SHA=$(cd ${MY_DIR} && git rev-parse HEAD)
+export SHA=$(cd "${MY_DIR}" && git rev-parse HEAD)
 
-. ${MY_DIR}/env.test
-. ${MY_DIR}/env.common
+. "${MY_DIR}"/env.test
+. "${MY_DIR}"/env.common
 
-${MY_DIR}/sh/docker_containers_down.sh
-${MY_DIR}/sh/build_docker_images.sh
-${MY_DIR}/sh/docker_containers_up.sh
-${MY_DIR}/katas_old/tar_pipe_in.sh
-${MY_DIR}/sh/run_tests_in_containers.sh ${*}
-if [ $? -eq 0 ]; then
-  ${MY_DIR}/sh/docker_containers_down.sh
+"${MY_DIR}"/sh/docker_containers_down.sh
+"${MY_DIR}"/sh/build_docker_images.sh
+"${MY_DIR}"/sh/docker_containers_up.sh
+"${MY_DIR}"/katas_old/tar_pipe_in.sh
+if "${MY_DIR}"/sh/run_tests_in_containers.sh "$@"; then
+  "${MY_DIR}"/sh/docker_containers_down.sh
 fi
