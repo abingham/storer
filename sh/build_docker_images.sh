c666eb91ca8f9e1b5142e40afbbf6f6e22f14d5c
Jon Jagger <jon@jaggersoft.com>
2018-04-29 16:16:15 +0100

    refactoring; bash quote style

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -3,10 +3,10 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-. ${ROOT_DIR}/env.test
-. ${ROOT_DIR}/env.common
+. "${ROOT_DIR}/env.test"
+. "${ROOT_DIR}/env.common"
 
 docker-compose \
-  --file ${ROOT_DIR}/docker-compose.yml \
-  --file ${ROOT_DIR}/docker-compose.test.yml \
+  --file "${ROOT_DIR}/docker-compose.yml" \
+  --file "${ROOT_DIR}/docker-compose.test.yml" \
     build
--- sh/docker_containers_down.sh
+++ sh/docker_containers_down.sh
@@ -2,8 +2,8 @@
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-. ${ROOT_DIR}/env.test
-. ${ROOT_DIR}/env.common
+. "${ROOT_DIR}/env.test"
+. "${ROOT_DIR}/env.common"
 
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
